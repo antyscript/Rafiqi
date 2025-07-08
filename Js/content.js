a9d4d42d18fa8b8f0ef710adc124a32a8e06808f
antyscript <antyscript@gmail.com>
2025-07-08 13:51:16 +0100

    remove fils that i have to change

--- Js/content.js
+++
@@ -1,5 +0,0 @@
-let dataprofil = JSON.parse(localStorage.getItem('dataprofil'));
-if (dataptofil.profil.username == '' && dataptofil.profil.password == ''){
-  window.location.href = 'login.html';
-  document.write('hello world');
-};
